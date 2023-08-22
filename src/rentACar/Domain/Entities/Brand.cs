418ee3397b496e6341ac099d9de634362630b1d8
CEMAL YILDIRIM <bystrong@technovadi.com>
2023-08-22 22:03:53 +0300

    Core.Persistence

---
+++ src/rentACar/Domain/Entities/Brand.cs
@@ -0,0 +1,14 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Brand : Entity<Guid>
+{
+    public string Name { get; set; } = default!;
+
+    public Brand(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
