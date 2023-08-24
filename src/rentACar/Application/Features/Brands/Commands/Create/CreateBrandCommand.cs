35e6e9d317bf2a4fa1a04f4fc21d7ae1826c78d2
CEMAL YILDIRIM <bystrong@technovadi.com>
2023-08-25 00:35:38 +0300

    `CreateBrandCommand`

---
+++ src/rentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace Application.Features.Brands.Commands.Create;
+
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>
+{
+    public string Name { get; set; } = default!;
+
+    public class CreateBrandCommandHandler
+        : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+    {
+        public Task<CreatedBrandResponse> Handle(
+            CreateBrandCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+            CreatedBrandResponse response = new() { Id = Guid.NewGuid(), Name = request.Name };
+
+            return Task.FromResult(response);
+        }
+    }
+}
--- src/rentACar/Domain/Entities/Brand.cs
+++ src/rentACar/Domain/Entities/Brand.cs
@@ -4,7 +4,7 @@ namespace Domain.Entities;
 
 public class Brand : Entity<Guid>
 {
-    public string Name { get; set; } = default!;
+    public string Name { get; set; }
 
     public Brand(Guid id, string name)
     {
